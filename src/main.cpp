d927f247b23fa1d31a0c7431e63dcdbe93f67af3
Niklas Bönisch <niklas.boenisch@gmail.com>
2019-04-14 09:15:38 +0200

    Vorm Aufräumen

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include <Wire.h>
 #include <WiFiUdp.h>
 #include <ESP8266WiFi.h>
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+#include <PID_v1.h>
+#include "MPU6050_6Axis_MotionApps20.h"
 
 
 
@@ -11,6 +15,8 @@ const uint8_t MPU6050SlaveAddress = 0x68;
 // Select SDA and SCL pins for I2C communication 
 const uint8_t scl = D4;
 const uint8_t sda = D3;
+
+#define absolut(x)   ( ((x) < 0)?  -(x)  :  (x) )
 
 // sensitivity scale factor respective to full scale setting provided in datasheet 
 const double AccelScaleFactor = 16384;
@@ -51,9 +57,11 @@ double ax,az;
 //
 // PID VALUES
 //
-double P = 0.01;
-double I = 0.01;
-double D = 0.01;
+double angle = 0;
+
+double P = 80;
+double I = 5;
+double D = 5;
 double soll = 0;
 
 
@@ -69,23 +77,60 @@ double soll = 0;
 #define MOTOR_DECEL 1000
 
 // Microstepping mode. If you hardwired it to save pins, set to the same value here.
-#define MICROSTEPS 1
+#define MICROSTEPS 4
 
 //MOTOR 1
-#define DIR 4
+#define DIR 5
 #define STEP 16
-#include "DRV8825.h"
-DRV8825 stepper(MOTOR_STEPS, DIR, STEP);
+AccelStepper ac1(AccelStepper::DRIVER,STEP,DIR);
+// MOTOR 2
+#define DIR2 12
+#define STEP2 14
+AccelStepper ac2(AccelStepper::DRIVER,STEP2,DIR2);
 
-
+MultiStepper steppers;
 ////
 //// STEPPER TEST
 ////
-#include <AccelStepper.h>
 
-AccelStepper s(AccelStepper::DRIVER,14,12);
+/*
+#include "DRV8825.h"
+//MOTOR 1
+#define DIR 5
+#define STEP 16
+// MOTOR 2
+#define DIR2 12
+#define STEP2 14
+DRV8825 s1(200, DIR, STEP);
+DRV8825 s2(200, DIR2, STEP2);
+*/
 
 
+//
+// Gyro
+//
+MPU6050 mpu;
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
 
 
 // PID Storage
@@ -95,6 +140,10 @@ double e[] = {0,0,0};
 //
 long lastTime = 0;
 
+double controll = 0;
+
+PID pid(&angle, &controll, &soll, P,I,D,DIRECT);
+
 //Prototypen Funktionen
 double calcAngle();
 void readAccelXZ();
@@ -102,73 +151,100 @@ void I2C_Write(uint8_t deviceAddress, uint8_t regAddress, uint8_t data);
 void initGyro();
 void initNetwork();
 double calcError(double soll, double ist);
-double calcPID(double error);
+float calcPID(double error);
 void initMultiplexer();
-void readMultiplexer(double *p, double *i, double *d);
+void readMultiplexer(double *p, double *i, double *d, double *v);
+void initGPIO();
 
 //PID Values
-double calcA(double Ts);
-double calcB(double Ts);
-double calcC(double Ts);
+float calcA(double Ts);
+float calcB(double Ts);
+float calcC(double Ts);
 
-double samplingTime();
+long samplingTime();
 
-
+void initGPIO()
+{
+  pinMode(STEP, OUTPUT);
+  pinMode(STEP2, OUTPUT);
+  pinMode(DIR, OUTPUT);
+  pinMode(DIR2, OUTPUT);
+}
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
   Wire.begin(sda, scl);
+  Wire.setClock(400000);
+  initGPIO();
   initGyro();
   //initNetwork();
   initMultiplexer();
   lastTime = millis();
+  pid.SetMode(AUTOMATIC);
+  pid.SetSampleTime(50);
+  pid.SetOutputLimits(-1500,1500);
 
-  s.setMaxSpeed(500);
+  ac1.setMaxSpeed(2000);
+  ac2.setMaxSpeed(2000);
+
+  ac1.setAcceleration(10000);
+  ac2.setAcceleration(10000);
+
+  //ac1.setSpeed(1500);
+  //ac2.setSpeed(-1500);
+
+  
+
+  //s1.begin(20, 4);
+  //s2.begin(20,4);
+
+  //s.setMaxSpeed(500);
   //s.run();
   //stepper.startRotate(65000);  
-  
+  /*
+  steppers.addStepper(s1);
+  steppers.addStepper(s2);*/
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //Serial.println(calcAngle());
-  double angleteest = calcAngle();
-  if(angleteest != 0)
-    s.setSpeed(300/calcAngle());
-  else
-    s.setSpeed(0);
-  
-  s.runSpeed();
-/*
-  double angle = calcAngle();
-  double error = calcError(soll, angle);
-  //error = 5;
-  double test = calcPID(error);
-  Serial.print(error); Serial.print(" "); Serial.println(test);
+  long time = millis();
+
+  angle = calcAngle();
+
+  pid.Compute();
+
+  ac1.setSpeed(controll);
+  ac2.setSpeed(-controll);
+
+  //Serial.print(angle); Serial.print(" "); Serial.print(controll); Serial.print(" P= ");Serial.print(P); Serial.print(" I= ");Serial.print(I); Serial.print(" D="); Serial.println(D);// Serial.println(samplingTime());
 
   double p = 0;
   double i = 0;
   double d = 0;
+  double v = 0;
 
-  readMultiplexer(&p,&i,&d);
+  readMultiplexer(&p,&i,&d, &v);
 
-  P = p;
-  I = i;
-  D = d;
-/*
-  Serial.print("P= "); Serial.print(p);
-  Serial.print(" I= "); Serial.print(i);
-  Serial.print(" D= "); Serial.println(d);
-  /*
-  stepper.setRPM(((int)test/abs(test)*60));
-  long timer = millis() + abs(test)/10;
-  while(timer > millis()){
-      stepper.nextAction();
-  }
-  */
+  //Serial.println(3*v/1024);
+  //Serial.println(v);
+
+  P = 30*p/1024;
+  I = 30*i/1024;
+  D = d*30/1024;
+
+  pid.SetTunings(P,I,D); 
+  ac1.runSpeed();
+  ac2.runSpeed();
+
+ while(time + 50>millis())
+ {
+   ac1.runSpeed();
+   ac2.runSpeed();
+ }
+ 
 }
 
 void initNetwork()
@@ -181,19 +257,37 @@ void initNetwork()
   Serial.println(udp.localPort());
 }
 
-double calcPID(double error)
+
+
+float calcPID(double error)
 {
   e[2] = e[1];
   e[1] = e[0];
   e[0] = error;
   double Ts = samplingTime();
-  double temp = uold + calcA(Ts)*e[0]+calcB(Ts)*e[1] + calcC(Ts)*e[2];
+  double temp = uold + (calcA(Ts)*e[0])+(calcB(Ts)*e[1]) + (calcC(Ts)*e[2]);
   uold = temp;
   return temp;
 }
 
 double calcAngle()
 {
+  fifoCount = mpu.getFIFOCount();
+
+  while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+  mpu.getFIFOBytes(fifoBuffer, packetSize);
+  fifoCount -= packetSize;
+
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+  mpu.dmpGetEuler(euler, &q);
+  Serial.print("euler\t");
+  Serial.print(euler[0] * 180/PI);
+  Serial.print("\t");
+  Serial.print(euler[1] * 180/PI);
+  Serial.print("\t");
+  Serial.println(euler[2] * 180/PI);
+
   
   readAccelXZ();
 /*
@@ -249,7 +343,7 @@ void I2C_Write(uint8_t deviceAddress, uint8_t regAddress, uint8_t data)
 }
 
 void initGyro()
-{
+{/*
   I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SMPLRT_DIV, 0x07);
   I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_1, 0x01);
   I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_2, 0x00);
@@ -264,7 +358,20 @@ void initGyro()
   Wire.beginTransmission(0x68);
   Wire.write(0x1A);  // the config address
   Wire.write(0x06);  // the config value
-  Wire.endTransmission(true);
+  Wire.endTransmission(true);*/
+
+  mpu.initialize();
+
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1688); // 1688 factory default for my test chip
+
+  mpu.setDMPEnabled(true);
+
 }
 
 void initMultiplexer()
@@ -275,29 +382,29 @@ void initMultiplexer()
   pinMode(S1, OUTPUT);
 }
 
-double calcA(double Ts)
+float calcA(double Ts)
 {
-  return (P+ I*Ts/2 + D/Ts);
+  return (P+ (I*Ts/2) + (D/Ts));
 }
 
-double calcB(double Ts)
+float calcB(double Ts)
 {
-  return (-P+I*Ts/2-2*D/Ts);
+  return (-P+(I*Ts/2)-(2*D/Ts));
 }
 
-double calcC(double Ts)
+float calcC(double Ts)
 {
   return D/Ts;
 }
 
-double samplingTime()
+long samplingTime()
 {
   long temp = lastTime;
   lastTime = millis();
-  return lastTime - temp;
+  return (lastTime - temp);
 }
 
-void readMultiplexer(double *p, double *i, double *d)
+void readMultiplexer(double *p, double *i, double *d, double *v)
 {
   //P
   digitalWrite(S0, LOW);
@@ -313,4 +420,9 @@ void readMultiplexer(double *p, double *i, double *d)
   digitalWrite(S0, LOW);
   digitalWrite(S1, HIGH);
   *d = analogRead(A0);
+
+  //Spannung V
+  digitalWrite(S0, HIGH);
+  digitalWrite(S1, HIGH);
+  *v = analogRead(A0);
 }
